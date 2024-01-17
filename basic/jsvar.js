499f3170691b71277dfb07fbe7408ddbaec235be
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-01-17 10:31:29 +0000

    datatype revision

--- basic/jsvar.js
+++ basic/jsvar.js
@@ -1,1 +1,16 @@
-const 
+// Datatype revision;
+//Primitive 
+
+//7 types:-String , Number , Boolean , null , undefined  ,  Symbol ,  BigInt ;
+
+
+//Reference datatypet(Non Primitive)
+//Array ,  Objects , Function;
+const myclass=["saktiman","naagraj","doga"];
+let class={
+    name="pranab",
+    age=19,
+}
+const myFunction =function(){
+    console.log("hello world");
+}
