d5be03c5fbe713d5bfcf288d94aec40a0cf03a80
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-01-17 09:50:48 +0000

    new day

--- basic/newvariable/var.js
+++ basic/newvariable/var.js
@@ -1,3 +1,29 @@
 const accountId=144553;
 let accountEmail="pranab.com";
-console.table([accountId,accountEmail]);
+//console.table([accountId,accountEmail]);
+
+let score =true;
+console.log(typeof score);
+
+let variableInNumber= Number(score);
+console.log(typeof variableInNumber);
+console.log(variableInNumber);
+//this is the notes when we change the string into number
+//"33"=>number;
+//"33abc"=>NaN;
+//null=>0;
+//undefined=>NaN;
+//true=1;
+//false=0;
+
+
+//now we try to change different datatype into boolean;
+let isLoggedIn = "pranab";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1=true;
+//0=false;
+//""=false;
+//"pranab"=true;
+
+
