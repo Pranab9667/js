f5150b064dfa1664822499a6e7dbca011446332f
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-01-18 10:47:06 +0000

    string js

---
+++ basic/string.js
@@ -0,0 +1,11 @@
+const name ="pranab";
+const  age = 19;
+
+
+//old way to print or write a string operation but now,we use(``);
+ console.log(`hello my name is ${name} and my age is  ${age}`);
+
+ //their are various type of string operation so we need to learn from different website of string operation ;
+ //slice ,  substring ,  trim -delete all the unwanted space in the string , indexof  , charAt and many more;
+
+ // must important thing in any language;
