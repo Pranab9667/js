29b0ae2d483e7d8d7785ba23609240d4908ce974
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-01-18 10:29:58 +0000

    stack and heap

--- basic/new.js
+++ basic/new.js
@@ -1,1 +1,18 @@
-const accountState = 
+let myName = "youtubechannel";
+
+let anotherName= myName;
+anotherName = "pranabpradhan".          //changes in stack datatype occur in the copy variable;
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    emailId :"pranabpradhan@google.com",
+    Upi : "user@ybl"
+}
+
+let userTwo = userOne;
+userTwo.emailId = " pranabpradhanasdd@google.com";
+
+console.log(userOne.emailId);             //chages in heap datatype occurs in the real variable;
+console.log(userTwo.emailId);
