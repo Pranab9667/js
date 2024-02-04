0dc5c40b6e7d5a588af3a283d2820b987007b1fa
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-02-04 04:00:44 +0000

    loop

---
+++ loop.js
@@ -0,0 +1,10 @@
+//Iteration  in js
+//for loop
+
+for (let i=0;i<10;i++){
+    const element =i;
+    if(element==5){
+        console.log("5 is the first number");
+    }
+    console .log(element);
+}
