bea5aeff78cd3411e90470e2bb6370c60b42e780
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-02-04 07:24:55 +0000

    maps

--- loop.js
+++ loop.js
@@ -8,3 +8,76 @@ for (let i=0;i<10;i++){
     }
     console .log(element);
 }
+
+
+let array =["flash","superman","batman"]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+//break and continue 
+
+//1.break
+for(let i=1;i<=20;i++){
+    if(i==5){
+        console.log(`detected 5`);
+        break
+    }
+    console.log(`value of the i is ${i}`);
+}
+//2.continue
+
+for(let i=1;i<=20;i++){
+    if(i==5){
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of the i is ${i}`);
+}
+
+
+
+//while and do while loop
+let i=0;
+while(i<=10){
+   console.log(`value of the is ${i}`);
+   i++;
+}
+
+let myArray =["flash","superman","ironman"];
+let a =0;
+while(a<myArray.length){
+    console.log(`${myArray[a]}`);
+    a++;
+}
+
+
+
+//do while loop
+let score =1;
+do{
+   console .log(`score is ${score}`)
+   score++;
+}while(score<10)
+
+
+// other variety in loops
+
+//for of 
+const arr=[1,2,3,4,5]
+for (const num of arr) {
+    console.log(`value of num ${num}`);
+}
+
+// Maps 
+
+const map =new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('FI',"France")
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+}
