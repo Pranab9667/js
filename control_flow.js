6c38b38c4f909fbd9df41ee1b90a59d87bffa0eb
Pranab9667 <143258526+Pranab9667@users.noreply.github.com>
2024-02-04 03:30:14 +0000

    switch

--- control_flow.js
+++ control_flow.js
@@ -4,4 +4,49 @@ if(condition )//if true
 {
     // enter in the condition statement
 }
-//<,>,<=,>=,==,!=,===,!==
+//<,>,<=,>=,==,!=,===,!==,&& (and operator),||(or operator)
+
+//switch case
+
+const month =3 
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+    case 9:
+        console.log("sep");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 12:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+
+    default:
+        break;
+}
